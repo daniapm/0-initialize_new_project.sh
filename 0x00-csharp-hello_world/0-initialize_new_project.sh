020ad5195f4c793229d5a97d26c5e98dd7243ec0
Dania <daniajibellis@hotmail.com>
2021-11-29 15:20:19 -0500

    Bash script that initializes a new C# project inside a folder titled 0-new_project

---
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Bash script that initializes a new C# project inside a folder titled 0-new_project
+
+mkdir 0-new_project
+cd 0-new_project
+dotnet new console
+dotnet build
+dotnet run
