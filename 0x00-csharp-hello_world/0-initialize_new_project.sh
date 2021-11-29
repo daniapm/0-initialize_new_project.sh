427189993a827f7f223dd29176e71151e600070f
Dania <daniajibellis@hotmail.com>
2021-11-29 16:33:40 -0500

    fix code

--- 0x00-csharp-hello_world/0-initialize_new_project.sh
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 # Bash script that initializes a new C# project inside a folder titled 0-new_project
-if [ ! -d /0-new_project ];
-then
-mkdir 0-new_project
-cd 0-new_project
-dotnet new console
-fi
+dotnet new console -o 0-new_project
