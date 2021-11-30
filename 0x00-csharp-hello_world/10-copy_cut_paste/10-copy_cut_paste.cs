8a098316ed6486563c49e37688e95b99b66ea36e
Dania <daniajibellis@hotmail.com>
2021-11-29 20:47:48 -0500

    print sring

--- 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -8,7 +8,6 @@ class Program
 		String word_first_3 = word.Substring(0, 3);
 		String word_last_2 = word.Substring(7);
 		String middle_word = word.Substring(1, 7);
-		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 		Console.WriteLine("First 3 letters: {0}", word_first_3);
 		Console.WriteLine("Last 2 letters: {0}", word_last_2);
 		Console.WriteLine("Middle word: {0}", middle_word);
