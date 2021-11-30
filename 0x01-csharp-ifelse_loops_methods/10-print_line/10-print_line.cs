1556afc6b2c9c2a5108cf00b7199d8fdf70284c1
Dania <daniajibellis@hotmail.com>
2021-11-30 17:46:53 -0500

    print line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Line
+{
+	public static void PrintLine(int length)
+	{
+		int i = 0;
+		for (i = 0; i < length; i++)
+		{
+			if (length <= 0)
+			{
+				Console.Write("\n");
+			}
+			else
+			{
+				Console.Write("_");
+
+			}
+		}
+		Console.Write("\n");
+
+	}
+}
+
