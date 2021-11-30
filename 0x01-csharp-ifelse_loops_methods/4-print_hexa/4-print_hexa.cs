916bb046e0865e3c579b4e9cd723bc61ff085f79
Dania <daniajibellis@hotmail.com>
2021-11-30 14:41:49 -0500

    print hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int i = 10;
+			for (i = 0; i <= 98; i++)
+			{
+				Console.WriteLine("{0} = 0x{0:x}", i);
+			}
+
+		}
+	}
+}
