33ff21b82bb0521c8fa72369c7f0ec088ef43b63
Dania <daniajibellis@hotmail.com>
2021-11-30 14:48:54 -0500

    print the number 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int firts = 0;
+			int last = 0;
+			for (firts = 0; firts <= 9; firts++)
+			{
+				for (last = 0; last <= 9; last++)
+				{
+					if (last == 9 && firts == 9)
+					{
+						Console.Write($"{firts}{last}");
+					}
+					else
+					{
+						Console.Write($"{firts}{last}, ");
+					}
+
+				}
+
+			}
+		}
+	}
+}
