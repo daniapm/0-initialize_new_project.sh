ba3de87a918f3f7081c318f5d35bbc24e1725cab
Dania <daniajibellis@hotmail.com>
2021-11-30 14:27:20 -0500

    fix code

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -9,15 +9,15 @@ class Program
 		int last = Math.Abs(number % 10);
 		if (last > 5)
 		{
-			Console.WriteLine($"The last digit of {number} is {last} and is greater than 5");
+			Console.Write($"The last digit of {number} is {last} and is greater than 5");
 		}
 		else if (last == 0)
 		{
-			Console.WriteLine($"The last digit of {number} is {last} and is 0");
+			Console.Write($"The last digit of {number} is {last} and is 0");
 		}
 		else if (last != 0 && last < 6)
 		{
-			Console.WriteLine($"The last digit of {number} is {last} and is less than 6 and not 0");
+			Console.Write($"The last digit of {number} is {last} and is less than 6 and not 0");
 		}
 	}
 }
