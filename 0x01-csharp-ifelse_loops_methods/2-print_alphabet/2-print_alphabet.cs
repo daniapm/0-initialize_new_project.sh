fa3914c3d58bbe96a4df24f397d4c29b7c952242
Dania <daniajibellis@hotmail.com>
2021-11-30 14:15:51 -0500

    print alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char letra = 'a';
+			for (letra = 'a'; letra <= 'z'; letra++)
+			{
+				Console.Write(letra);
+			}
+		}
+
+	}
+}
