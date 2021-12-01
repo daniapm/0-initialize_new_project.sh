c37c7bc3e3f321815126604b91b35c773082574c
Dania <daniajibellis@hotmail.com>
2021-11-30 20:38:47 -0500

    print /

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Line
+{
+	public static void PrintDiagonal(int length)
+	{
+		int i = 0;
+		int j = 0;
+		if (length > 0)
+		{
+			for (i = 0; i < length; i++)
+			{
+				for (j = 0; j < i; j++)
+				{
+					Console.Write(" ");
+				}
+				Console.WriteLine("\\");
+
+			}
+		}
+		Console.WriteLine("");
+
+	}
+
+}
