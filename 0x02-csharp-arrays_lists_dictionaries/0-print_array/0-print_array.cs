6edd6be6b61b06de7bb7ae6093abe6ba9b8faf15
Dania <daniajibellis@hotmail.com>
2021-12-01 13:43:52 -0500

    print array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Array
+{
+	public static int[] CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+
+
+		}
+		int[] array1 = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			array1[i] = i;
+		}
+
+		for (int j = 0; j < size; j++)
+		{
+			if (size != 0)
+			{
+				if (j == array1.Length)
+				{
+					Console.Write(j);
+				}
+				else
+				{
+					Console.Write(j + " ");
+				}
+
+			}
+			else
+			{
+				Console.WriteLine();
+			}
+
+		}
+		Console.WriteLine();
+		return array1;
+
+	}
+
+}
