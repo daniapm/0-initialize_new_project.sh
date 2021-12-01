abe602eee679f6d9056080f888f0815c58dbc2d8
Dania <daniajibellis@hotmail.com>
2021-12-01 14:01:19 -0500

    print array for index

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -5,7 +5,7 @@ class Array
 	{
 		if (index < 0 || index > array.Length - 1)
 		{
-			Console.Write("Index out of range");
+			Console.WriteLine("Index out of range");
 			return -1;
 		}
 		return array[index];
