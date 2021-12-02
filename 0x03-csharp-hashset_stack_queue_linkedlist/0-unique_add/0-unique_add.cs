7138668d735e1f77bc1d0cdd4776fb1322619b28
Dania <daniajibellis@hotmail.com>
2021-12-02 11:13:49 -0500

    adds all unique integers in a list.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		HashSet<int> set = new HashSet<int>(myList);
+		int total = set.Sum();
+		return total;
+	}
+
+}
