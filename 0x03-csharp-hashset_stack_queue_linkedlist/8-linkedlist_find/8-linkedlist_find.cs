ab9c2f708098d20a03f924d6e7be633f0681c870
Dania <daniajibellis@hotmail.com>
2021-12-02 15:55:06 -0500

    find index in linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+	public static int FindNode(LinkedList<int> myLList, int value)
+	{
+		int valor = 0;
+		for (int element = 0; element < myLList.Count; element++)
+		{
+			if (element == value)
+			{
+				return (valor);
+			}
+		}
+		return (-1);
+	}
+}
