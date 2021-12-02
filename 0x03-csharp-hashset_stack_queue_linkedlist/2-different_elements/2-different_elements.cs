966a04e7ff607b0f51aa81feb922db92a5ca1138
Dania <daniajibellis@hotmail.com>
2021-12-02 11:56:20 -0500

    print different elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class List
+{
+	public static List<int> DifferentElements(List<int> list1, List<int> list2)
+	{
+		List<int> total = list1.Where(i => !list2.Contains(i)).ToList();
+		List<int> total2 = list2.Where(i => !list1.Contains(i)).ToList();
+		total.AddRange(total2);
+		total.Sort();
+		return total;
+	}
+
+}
