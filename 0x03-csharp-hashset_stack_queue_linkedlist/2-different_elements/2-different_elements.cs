1bf20762a786482326927c6f45bd83bc49feb8ce
Dania <daniajibellis@hotmail.com>
2021-12-02 14:26:26 -0500

    fix code

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -4,8 +4,8 @@ class List
 {
 	public static List<int> DifferentElements(List<int> list1, List<int> list2)
 	{
-		List<int> total = list1.Where(i => !list2.Contains(i)).ToList();
-		List<int> total2 = list2.Where(i => !list1.Contains(i)).ToList();
+		List<int> total = list1.FindAll(i => !list2.Contains(i)).ToList();
+		List<int> total2 = list2.FindAll(i => !list1.Contains(i)).ToList();
 		total.AddRange(total2);
 		total.Sort();
 		return total;
