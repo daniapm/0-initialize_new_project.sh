4ebbab9c47b646154b763b2402996711814388ed
Dania <daniajibellis@hotmail.com>
2021-12-02 11:33:59 -0500

    print common elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+	{
+
+		List<int> total = list1.FindAll(element => list2.Contains(element));
+		return total;
+	}
+
+}
