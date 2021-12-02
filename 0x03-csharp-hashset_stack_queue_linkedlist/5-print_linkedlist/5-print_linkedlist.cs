d571eb1da9f3b7d1170992f08b2e5862ffd427e1
Dania <daniajibellis@hotmail.com>
2021-12-02 14:58:13 -0500

    print linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+
+		LinkedList<int> linked = new LinkedList<int>();
+		if (size < 0)
+		{
+			return linked;
+		}
+
+		for (int i = 0; i < size; i++)
+		{
+			linked.AddFirst(i);
+			Console.WriteLine(i);
+		}
+		return linked;
+	}
+
+}
