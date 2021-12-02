f4384cfa7169b25b8bd442132d8fc856508d7230
Dania <daniajibellis@hotmail.com>
2021-12-02 15:13:39 -0500

    print length to linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Length(LinkedList<int> myLList)
+	{
+		var suma = 0;
+		for (int i = 0; i < myLList.Count; i++)
+		{
+			suma++;
+
+		}
+		return suma;
+	}
+
+}
