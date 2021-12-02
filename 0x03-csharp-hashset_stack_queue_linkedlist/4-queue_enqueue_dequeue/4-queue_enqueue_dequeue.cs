c359ff7e73da597aa34adcd2c62558a3bb2a8614
Dania <daniajibellis@hotmail.com>
2021-12-02 14:40:59 -0500

    print queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+	{
+		if (aQueue.Count <= 0)
+		{
+			Console.WriteLine("Stack is empty");
+		}
+		else
+		{
+			Console.WriteLine($"Number of items: {aQueue.Count}");
+
+		}
+		Console.WriteLine($"Top item: {aQueue.Peek()}");
+		if (aQueue.Contains(search))
+		{
+			Console.WriteLine($"Stack contains \"{search}\": True");
+			while (aQueue.Dequeue() != search) ;
+		}
+		else
+		{
+			Console.WriteLine($"Stack contains \"{search}\": False");
+		}
+		aQueue.Enqueue(newItem);
+		return aQueue;
+	}
+
+}
