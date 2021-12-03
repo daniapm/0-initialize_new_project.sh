ca5475a688563d2713305515462c3d5f41fe9ae4
Dania <daniajibellis@hotmail.com>
2021-12-03 16:11:49 -0500

    priny list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+		}
+		List<int> mylist = new List<int>(size);
+		for (int i = 0; i < size; i++)
+		{
+			mylist.Add(i);
+			if (i == size)
+			{
+				Console.Write(i);
+			}
+			else
+			{
+				Console.Write($"{i} ");
+			}
+
+		}
+		Console.WriteLine();
+		return mylist;
+	}
+
+}
