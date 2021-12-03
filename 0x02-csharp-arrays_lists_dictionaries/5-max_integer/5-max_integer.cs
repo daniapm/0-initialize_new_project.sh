62879ea1c050cd87fb22dd2ca340b4796e29dabf
Dania <daniajibellis@hotmail.com>
2021-12-03 18:14:18 -0500

    print the last element to list

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+	public static int MaxInteger(List<int> myList)
+	{
+		if (myList.Count < 0)
+		{
+			System.Console.WriteLine("List is empty");
+		}
+		int last = 0;
+		for (int i = 0; i < myList.Count; i++)
+		{
+			myList.Sort();
+			last = myList[myList.Count - 1];
+		}
+		return last;
+	}
+
+}
