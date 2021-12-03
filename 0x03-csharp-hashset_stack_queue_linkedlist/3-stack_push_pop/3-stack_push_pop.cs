d47daf62efa6d4126d963a2958de1c1bddf092a4
Dania <daniajibellis@hotmail.com>
2021-12-02 21:39:18 -0500

    fix code

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,16 +5,17 @@ class MyStack
 {
 	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
 	{
+
+		Console.WriteLine($"Number of items: {aStack.Count}");
 		if (aStack.Count <= 0)
 		{
 			Console.WriteLine("Stack is empty");
 		}
+		else
+		{
+			Console.WriteLine($"Top item: {aStack.Peek()}");
 
-		Console.WriteLine($"Number of items: {aStack.Count}");
-
-
-
-		Console.WriteLine($"Top item: {aStack.Peek()}");
+		}
 		if (aStack.Contains(search))
 		{
 			Console.WriteLine($"Stack contains \"{search}\": True");
