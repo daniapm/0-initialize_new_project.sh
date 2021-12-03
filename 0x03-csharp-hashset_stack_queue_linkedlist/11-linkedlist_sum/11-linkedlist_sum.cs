49bb7dcc20650728d24345dcd0a8612bad632830
Dania <daniajibellis@hotmail.com>
2021-12-02 20:22:36 -0500

    add element to linkedlist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+	public static int Sum(LinkedList<int> myLList)
+	{
+		int suma = 0;
+		foreach (int item in myLList)
+		{
+			suma = suma + item;
+
+		}
+		suma++;
+
+		return suma;
+	}
+
+}
