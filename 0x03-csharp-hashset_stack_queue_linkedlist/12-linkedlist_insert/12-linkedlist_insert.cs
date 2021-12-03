5773c6bb8f62b627acb5336ed4af8c781bcf218e
Dania <daniajibellis@hotmail.com>
2021-12-02 20:50:06 -0500

    insert linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		LinkedListNode<int> dummy = myLList.First;
+		if (dummy != null)
+		{
+			if (n < dummy.Value)
+			{
+				return myLList.AddBefore(dummy, n);
+			}
+			dummy = dummy.Next;
+		}
+		return myLList.AddLast(n);
+	}
+
+}
