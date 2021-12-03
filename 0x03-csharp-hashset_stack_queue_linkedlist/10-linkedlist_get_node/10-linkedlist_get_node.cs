c02a8eeca67e1f2c4f8c6dafe78d650072815366
Dania <daniajibellis@hotmail.com>
2021-12-02 19:46:43 -0500

    return node in the position

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		int element = 0;
+		foreach (int item in myLList)
+		{
+			if (element == n)
+			{
+				return item;
+
+
+			}
+			element++;
+		}
+		return 0;
+	}
+}
