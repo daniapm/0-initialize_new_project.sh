780cf4f089f95cd4e351cb373af2463eb5ad5723
Dania <daniajibellis@hotmail.com>
2021-12-05 21:55:47 -0500

    sorted dicctionary

--- 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -6,7 +6,7 @@ class Dictionary
 	public static void PrintSorted(Dictionary<string, string> myDict)
 	{
 		SortedList<string, string> sortedkeys = new SortedList<string, string>(myDict);
-		foreach (KeyValuePair<string, string> element in keys)
+		foreach (KeyValuePair<string, string> element in sortedkeys)
 			Console.WriteLine($"{element.Key}: {element.Value}");
 	}
 }
