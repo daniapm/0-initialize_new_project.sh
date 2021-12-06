d6cd2cf8a2da2a27964e69d10e7e04ebb42d76dc
Dania <daniajibellis@hotmail.com>
2021-12-05 22:30:59 -0500

    rectangular arrays

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[][] notas;
+		notas = new int[5][];
+		notas[0] = new int[5];
+		notas[1] = new int[5];
+		notas[2] = new int[5];
+		notas[3] = new int[5];
+		notas[4] = new int[5];
+
+		for (int i = 0; i < notas.Length; i++)
+		{
+			for (int j = 0; j < notas[i].Length; j++)
+			{
+				if (notas[i][j] == notas[2][2])
+					notas[2][2] = 1;
+				notas[i][j] = 0;
+			}
+		}
+		for (int i = 0; i < notas.Length; i++)
+		{
+			for (int j = 0; j < notas[i].Length; j++)
+			{
+				Console.Write(" {0}", notas[i][j]);
+			}
+			Console.WriteLine();
+		}
+
+	}
+}
