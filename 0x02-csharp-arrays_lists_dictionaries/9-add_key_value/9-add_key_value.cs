5e059c411eaa180abe3c5e2e81b6177af7bda38d
Dania <daniajibellis@hotmail.com>
2021-12-05 21:33:53 -0500

    adding element

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+	public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+	{
+		bool index = false;
+		foreach (KeyValuePair<string, string> entry in myDict)
+		{
+			if (entry.Key == key)
+				index = true;
+		}
+		if (index == true)
+			myDict[key] = value;
+		else
+			myDict.Add(key, value);
+		return myDict;
+	}
+
+}
