64e643da93ca4d520094318612b40b7bbaf916c4
Dania <daniajibellis@hotmail.com>
2021-12-05 21:20:55 -0500

    number keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+	public static int NumberOfKeys(Dictionary<string, string> myDict)
+	{
+		int contdor = 0;
+		foreach (var keys in myDict)
+		{
+			contdor = contdor + 1;
+
+		}
+		return contdor;
+
+
+	}
+}
