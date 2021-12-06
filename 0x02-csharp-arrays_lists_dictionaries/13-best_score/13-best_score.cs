2db16e99b117ab6432ea61e1ca6a6035dfd83202
Dania <daniajibellis@hotmail.com>
2021-12-05 22:13:41 -0500

    best score in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myList)
+	{
+		int val = 0;
+		string keys = "";
+
+		if (myList.Count == 0)
+		{
+			return "None";
+		}
+		foreach (KeyValuePair<string, int> element in myList)
+		{
+			if (element.Value >= val)
+			{
+				val = element.Value;
+				keys = element.Key;
+			}
+		}
+		return keys;
+	}
+}
