db707b69c62fca12575028145823cebe34b6df72
Dania <daniajibellis@hotmail.com>
2021-12-13 15:20:55 -0500

    print list

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int SafePrint(List<int> myList, int n)
+	{
+		int num = n;
+		try
+		{
+
+			for (int i = 1; i < num + 1; i++)
+			{
+				System.Console.WriteLine(i);
+
+			}
+
+
+		}
+		catch
+		{
+			System.Console.WriteLine("no se pudo");
+		}
+		return n;
+
+	}
+
+}
