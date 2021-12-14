33cc9744cd13f33c111881cb87003de914f92204
Dania <daniajibellis@hotmail.com>
2021-12-14 14:22:15 -0500

    create property

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Enemies
+{
+	class Zombie
+	{
+		private int health;
+		private string name = "(no name)";
+
+		public Zombie()
+		{
+			health = 0;
+		}
+		public Zombie(int value)
+		{
+
+			if (value >= 0)
+			{
+				health = value;
+			}
+			else
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		public int GetHealth()
+		{
+			return health;
+
+		}
+
+	}
+
+
+}
