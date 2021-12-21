98c024183404cb55a580faa0d8aace47061d55e7
daniapm <daniajibellis@hotmail.com>
2021-12-21 17:03:30 -0500

    Rectangle

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Shape
+{
+    public virtual int Area()
+    {
+       throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int wiedth;
+    private int height;
+
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            height = value;
+        }
+    }
+}
