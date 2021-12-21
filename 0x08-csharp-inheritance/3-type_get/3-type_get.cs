16ce8bcf6164e3eec95abf433c7f5ba86063884f
daniapm <daniajibellis@hotmail.com>
2021-12-21 16:36:53 -0500

    Type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Type type = myObj.GetType();
+        TypeInfo t = type.GetTupeInfo();
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        IEnumerable<MethodInfo> mList = t.GetMethods();
+
+        System.Console.WriteLine("{0} Properties:", t.Name);
+        foreach (PropertyInfo p in pList)
+        {
+            System.Console.WriteLine(p.Name);
+        }
+        
+        System.Console.WriteLine("{0} Methods:", t.Name);
+        foreach (MethodInfo m in mList)
+        {
+            System.Console.WriteLine(m.Name);
+        }
+    }
+}
