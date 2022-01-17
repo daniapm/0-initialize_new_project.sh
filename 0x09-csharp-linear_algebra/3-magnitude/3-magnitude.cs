86356fa185e3f4e2470d532d62c812541e1cbeb8
daniapm <daniajibellis@hotmail.com>
2022-01-17 11:43:32 -0500

    method that calculates and returns the length of a given vector

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return -1;
+        }
+        double result = 0.0;   
+        foreach (double index in vector)
+        {
+            result += (Math.Pow(index, 2));
+        }
+        return Math.Round(Math.Sqrt(result), 2);
+
+    }
+
+}
