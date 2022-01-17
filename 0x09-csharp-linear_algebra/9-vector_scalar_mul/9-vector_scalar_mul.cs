7582441218f9c53848369865381cdd228b28ed50
daniapm <daniajibellis@hotmail.com>
2022-01-17 14:41:57 -0500

    method that multiplies a vector and a scalar and returns the resulting vector

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] result = new double[vector1.Length];
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return new double[] {-1};
+        }
+        
+        for (int i = 0; i < vector.Length; i++)
+        {
+            result[i] = Math.Round(vector1[i] * scalar, 2);
+        }
+        return result;
+    }
+}
