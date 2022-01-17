1029db9775e5867c0e7c83409bf5fe7ef960be15
daniapm <daniajibellis@hotmail.com>
2022-01-17 15:04:25 -0500

    method that calculates dot product of either two 2D or two 3D vectors.

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length < 2 || vector1.Length > 3 || vector2.Length < 2 || vector2.Length > 3 || vector1.Length != vector2.Length)
+        {
+            return -1;
+        }
+        
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            result += (Math.Round(vector1[i] * vector2[i], 2));
+        }
+        return result;
+    }
+}
