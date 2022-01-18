a27cc3a2e25cb805c2be3857187f0e26fefba964
daniapm <daniajibellis@hotmail.com>
2022-01-18 16:18:29 -0500

    fix code

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -8,10 +8,10 @@ class MatrixMath
         double[,] mymatrix = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
 
         if (matrix1.GetLength(0) != matrix2.GetLength(0)
-        || matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3
-        || matrix2.GetLength(0) < 2 || matrix2.GetLength(0) > 3
-        || matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3
-        || matrix2.GetLength(1) < 2 || matrix2.GetLength(1) > 3)
+        || matrix1.GetLength(0) < 2|| matrix1.GetLength(0) > 3
+        || matrix2.GetLength(0) < 2|| matrix2.GetLength(0) > 3
+        || matrix1.GetLength(1) < 2|| matrix1.GetLength(1) > 3
+        || matrix2.GetLength(1) < 2|| matrix2.GetLength(1) > 3)
         {
             return new double [,] {{-1}};
         }
