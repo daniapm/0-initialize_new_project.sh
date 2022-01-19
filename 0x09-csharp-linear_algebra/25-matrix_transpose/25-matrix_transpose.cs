df7f6a8bf23bb7cb49e2b6f4cc3d6facab25013a
daniapm <daniajibellis@hotmail.com>
2022-01-19 10:35:41 -0500

    method to transpose a matrix and return the resulting matrix

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] mymatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
+
+        if (matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+        {
+            return new double [,] {{}};
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                mymatrix[j, i] = matrix[j, i];
+            }
+        }
+        return mymatrix;
+    }
+}
