1febe2e6a036a0125dfbd11b17c903c562bd8c9a
daniapm <daniajibellis@hotmail.com>
2022-01-20 09:01:32 -0600

    method that calculates the determinant of a matrix

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -5,20 +5,20 @@ class MatrixMath
 {
     public static double Determinant(double[,] matrix)
     {
-        int file = GetLength(0);
-        int colum = GetLength(1);
+        int file = matrix.GetLength(0);
+        int colum = matrix.GetLength(1);
 
-        if (matrix.file < 2|| matrix.file > 3
-        || matrix.colum < 2|| matrix.colum > 3)
+        if (file < 2|| file > 3
+        || colum < 2|| colum > 3)
         {
             return -1;
         }
-        else if (matrix.file == 2 && matrix.colum == 2)
+        else if (file == 2 && colum == 2)
         {
             det = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
             return(det);
         }
-        else if (matrix.file == 3 && matrix.colum == 3)
+        else if (file == 3 && colum == 3)
         {
             det = Math.Round((matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]))), 2);
             return(det);
