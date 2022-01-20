c32183138b4752c62d731855561430ab7916359d
daniapm <daniajibellis@hotmail.com>
2022-01-20 09:44:32 -0600

    method that calculates the cross product of two 3D vectors and returns the resulting vector.

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -5,6 +5,7 @@ class VectorMath
 {
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
+        
         int lenvector1 = vector1.Length;
         int lenvector2 = vector2.Length;
         
@@ -14,8 +15,9 @@ class VectorMath
         }
         double[] myvector = new double[lenvector1];
 
-        myvector = [vector1[1] * vector2[2] - vector1[2] * vector2[1], vector1[2] * vector2[0] - vector1[0] * vector2[2],
-        vector1[0] * vector2[1] - vector1[1] * vector2[0]];
+        myvector[0] = (vector1[1] * vector2[2] - vector1[2] * vector2[1]);
+        myvector[1] = (vector1[2] * vector2[0] - vector1[0] * vector2[2]);
+        myvector[2] = (vector1[0] * vector2[1] - vector1[1] * vector2[0]);
 
         return myvector;
     }
