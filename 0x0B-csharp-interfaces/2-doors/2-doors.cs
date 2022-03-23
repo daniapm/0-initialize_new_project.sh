1658fcd5a7bd830723d6d5c48fe4aa0cf77bb398
daniapm <daniajibellis@hotmail.com>
2022-03-22 19:18:47 -0600

    Create a new class called Door that inherits from Base and IInteractive

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Base
+{
+    public string name {
+        get;
+        set;
+    }
+
+    public override string ToString()
+    {
+        return ($"{this.name} is a {this.GetType()}");
+    }
+}
+
+interface IInteractive {
+
+    void Interact();
+}
+
+interface IBreakable {
+
+    int durability
+    {
+        get;
+        set;
+    }
+
+    void Break();
+
+}
+
+interface ICollectable {
+    bool isCollected {
+        get;
+        set;
+    }
+
+    void Collect();
+}
+
+class Door : Base, IInteractive {
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+
+    public string name(string name="Door") {
+        this.name = name;
+    }
+}
