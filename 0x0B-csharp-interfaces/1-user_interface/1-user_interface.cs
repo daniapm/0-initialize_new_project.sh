c34f36098c14880cb9987ab6cfe5d1729d2c236f
daniapm <daniajibellis@hotmail.com>
2022-03-22 19:03:35 -0600

    creating interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Base
+{
+    public string name {
+        get;
+        set;
+    }
+
+    public override string ToString()
+    {
+        return ($"{this.name} is a {this.GetType()}");
+    }
+}
+
+interface IInteractive {
+
+    void Interact();
+}
+
+interface IBreakable {
+
+    int durability
+    {
+        get;
+        set;
+    }
+
+    void Break();
+
+}
+
+interface ICollectable {
+    bool isCollected {
+        get;
+        set;
+    }
+
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable {
+
+    public int durability {
+        get;
+        set;
+    }
+
+    public bool isCollected {
+        get;
+        set;
+    }
+}
