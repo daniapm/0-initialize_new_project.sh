90a5013b9e1812fc39857c1c92f2e1dd26533ce6
daniapm <daniajibellis@hotmail.com>
2022-03-22 20:54:36 -0600

    Better iterating and acting

---
+++ 0x0B-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Base
+{
+    public string name {
+        get;
+        set;
+    }
+
+    public override string ToString()
+    {
+        return ($"{this.name} is a {this.GetType()}");
+    }
+}
+
+interface IInteractive {
+
+    void Interact();
+}
+
+interface IBreakable {
+
+    int durability
+    {
+        get;
+        set;
+    }
+
+    void Break();
+
+}
+
+interface ICollectable {
+    bool isCollected {
+        get;
+        set;
+    }
+
+    void Collect();
+}
+
+class Door : Base, IInteractive {
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+
+    public Door(string name="Door") {
+        this.name = name;
+    }
+}
+
+class Decoration : Base, IInteractive, IBreakable {
+
+    public bool isQuestItem {
+        get;
+        set;
+    }
+
+    public int durability {
+        get;
+        set;
+    }
+
+    public Decoration(string name="Decoration", int durability=1, bool isQuestItem=false)
+    {
+        if (durability <= 0) {
+            throw new Exception("Durability must be greater than 0");
+        }
+        else
+        {
+            this.durability = durability;
+            this.name = name;
+            this.isQuestItem = isQuestItem;
+        }
+    }
+
+    public void Interact()
+    {
+        if (this.durability <= 0) {
+            Console.WriteLine($"The {this.name} has been broken.");
+        }
+        else
+        {
+            if(isQuestItem == true) {
+                Console.WriteLine($"You look at the {this.name}. There's a key inside.");
+            }
+            else {
+                Console.WriteLine($"You look at the {this.name}. Not much to see here.");
+            }
+        }
+        
+    }
+
+    public void Break()
+    {
+        this.durability -= 1;
+        if (this.durability > 0)
+            Console.WriteLine($"You hit the {this.name}. It cracks.");
+        else if (durability == 0)
+            Console.WriteLine($"You smash the {this.name}. What a mess.");
+        else
+            Console.WriteLine($"The {this.name} is already broken.");
+    }
+}
+
+class Key : Base, ICollectable {
+
+    public bool isCollected {
+        get;
+        set;
+    }
+
+    public Key(string name="Key", bool isCollected=false) {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    public void Collect()
+    {
+        if (isCollected == false)
+        {
+            isCollected = true;
+            Console.WriteLine($"You pick up the {this.name}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have already picked up the {this.name}.");
+        }
+    }
+
+}
+
+class Objs<T> : IEnumerable<T> {
+
+    List<T> lista = new List<T>();
+
+    public void Add(T obj)
+    {
+        lista.Add(obj);
+    }
+	
+    public IEnumerator<T> GetEnumerator()
+    {
+        return lista.GetEnumerator();
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
