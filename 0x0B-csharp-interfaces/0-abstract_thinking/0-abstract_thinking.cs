17d3e6a27986ad37d68405e2c70bba35d9c931dd
daniapm <daniajibellis@hotmail.com>
2022-03-22 18:02:03 -0600

    adding abstaract class Base

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Base
+{
+    public string name {
+        get;
+        set;
+    }
+
+    public override string ToString()
+    {
+        return ($"{this.name} is a {this.GetType()}");
+    }
+}
