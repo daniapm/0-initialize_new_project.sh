42078e68248a5ba75c99fce846e3537aaf3544ff
daniapm <daniajibellis@hotmail.com>
2022-03-25 01:51:15 -0600

    fix values

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -10,11 +10,11 @@ public class Player
     public Player(string name="Player", float maxHp=100f) {
         
         if (maxHp <= 0f) {
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
         else {
-            this.maxHp = 100f;
+            this.maxHp = maxHp;
         }
         this.name = name;
         this.hp = this.maxHp;
