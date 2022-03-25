9fd35889cf577c6d0620a1276ff2df927dc8450e
daniapm <daniajibellis@hotmail.com>
2022-03-25 01:36:09 -0600

    Adding public class called Player

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Player
+{
+    private String name { get; set; }
+    private String maxHp { get; set; }
+    private String hp { get; set; }
+
+    public Player(string name="Player", float maxHp=100f) {
+        
+        if (maxHp > 0f) {
+            this.maxHp = maxHp;
+        }
+        else {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.")
+            this.maxHp = 100f;
+            
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth() {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
