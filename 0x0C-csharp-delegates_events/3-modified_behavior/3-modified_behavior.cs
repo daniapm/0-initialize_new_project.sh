ceddfeda56b9f08d4474c40e1a8ff8a5b013a9e6
daniapm <daniajibellis@hotmail.com>
2022-03-25 03:05:55 -0600

    Fix code

--- 0x0C-csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
+++ 0x0C-csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -61,17 +61,17 @@ class Player
                 this.hp = newHp;
             }
         }
-}
 
-public float ApplyModifier(float baseValue, Modifier modifier) {
-    if (modifier == Modifier.Weak) {
-        return baseValue * 0.5f;
-    }
-    else if (modifier == Modifier.Base) {
-        return baseValue;
-    }
-    else {
-        return baseValue * 1.5f;
+    public float ApplyModifier(float baseValue, Modifier modifier) {
+        if (modifier == Modifier.Weak) {
+            return baseValue * 0.5f;
+        }
+        else if (modifier == Modifier.Base) {
+            return baseValue;
+        }
+        else {
+            return baseValue * 1.5f;
+        }
     }
 }
 
