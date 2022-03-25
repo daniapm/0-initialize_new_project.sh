fe494193e196d31f36d57128f475ec39d7024b86
daniapm <daniajibellis@hotmail.com>
2022-03-25 02:28:37 -0600

    Adding delegate

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name="Player", float maxHp=100f) {
+        
+        if (maxHp <= 0f) {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth() {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+
+    delegate void CalculateHealth(float demage);
+
+    public void TakeDamage(float damage) {
+        if (damage < 0f) {
+            System.Console.WriteLine("{0} takes 0 damage!", this.name);
+            damage = 0f;
+        }
+        else {
+            System.Console.WriteLine("{0} takes {1} damage!", this.name, damage);
+        }
+        this.hp -= damage;
+        }
+
+        public void HealDamage(float heal) {
+            if (heal < 0f) {
+            System.Console.WriteLine("{0} heals 0 HP!", this.name);
+            heal = 0f;
+        }
+        else {
+            Console.WriteLine("{0} heals {1} HP!", this.name, heal);
+        }
+        this.hp += heal;
+        }
+}
