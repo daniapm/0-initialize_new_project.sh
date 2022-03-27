813b8daaa52dd69ba58a6fb6e7e5f2e4a1ec5615
daniapm <daniajibellis@hotmail.com>
2022-03-27 17:05:18 -0600

    new method Peek() to the class Queue<T> that returns the value of the first node of the queue without removing the node

---
+++ 0x0A-csharp-generics/3-peek/queue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Queue<T>
+{
+
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+
+	public class Node
+	{
+		public T value;
+		public Node next = null;
+
+		public Node(T val)
+		{
+			value = val;
+		}
+	}
+
+	public Node head;
+	public Node tail;
+	public int count;
+
+	public void Enqueue(T value)
+	{
+		Node newnode = new Node(value);
+		if (head == null)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail.next = newnode;
+			tail = newnode;
+		}
+		count++;
+	}
+
+	public int Count()
+	{
+		return count;
+	}
+
+    public T Dequeue()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			tail.value = head.value;
+			head = head.next;
+			count--;
+			return tail.value;
+		}
+	}
+
+    public Peek() {
+        if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+        else {
+            return head.value;
+        }
+    }
+}
