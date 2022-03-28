04ae7e82399e9916226a5642ee67f3895a6f3b92
daniapm <daniajibellis@hotmail.com>
2022-03-27 22:14:46 -0600

    new class called Node

---
+++ 0x0A-csharp-generics/1-enqueue/1-enqueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Queue<T>
+{
+
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+
+	public class Node
+	{
+		private T value;
+		public Node next = null;
+
+		public Node(T val)
+		{
+			value = val;
+		}
+	}
+
+	public Node head;
+	public Node tail;
+	public int count;
+
+	public void Enqueue(T value)
+	{
+		Node newnode = new Node(value);
+		if (head == null)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail.next = newnode;
+			tail = newnode;
+		}
+		count++;
+	}
+
+	public int Count()
+	{
+		return count;
+	}
+}
