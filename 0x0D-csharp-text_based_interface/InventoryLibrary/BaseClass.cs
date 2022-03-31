1f9015bd9979287df95f7bf42e719f7a8e2d7b96
daniapm <daniajibellis@hotmail.com>
2022-03-30 20:28:16 -0600

    Addin solution

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace InventoryLibrary
+{
+
+    class BaseClass
+    {
+        private string id;
+        private float date_created;
+        private float date_updated;
+
+        public BaseClass(string id = null; float date_created = default(Datetime), float date_updated =  default(Datetime))
+        {
+            if (id != null)
+                this.id = id;
+            else
+                this.id = Guid.NewGuid().ToString();
+
+            if (date_created != default(DateTime))
+                this.date_created = date_created;
+            else
+                this.date_created = DateTime.Now;
+        
+            if (date_updated != default(DateTime))
+                this.date_updated = date_updated;
+            else
+                this.date_updated = DateTime.Now;
+        }
+    }
+}
